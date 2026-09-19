32e9dadcb4ac9b9fcadb59faeba04af8de4b2dc8
vagrant <deforddm@gmail.com>
2026-09-19 01:08:17 -0700

    sw: activate new version immediately

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE = 'ashes-v2.0.0';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener('message', e => { if (e.data === 'skip') self.skipWaiting(); });
+self.addEventListener('message', e => { self.skipWaiting(); });
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
   if (url.origin !== location.origin) { e.respondWith(fetch(e.request).catch(() => new Response('', {status:504}))); return; }
